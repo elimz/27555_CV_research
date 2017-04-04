698858aaa9acb85d3e80192bdf732dfb06432464
elimz <yilinz@andrew.cmu.edu>
2017-04-04 08:40:29 -0400

    now as a submodule that writes maniped img to a folder, if called by top module

--- manip.py
+++ manip.py
@@ -28,11 +28,12 @@ def remove_noise (img): return
         # Aims to remove 1-pixel noise in the background 
 
 # def main(img_path, mask_path)
-def main():
+def main(img_path, mask_path):
     ## currently hard-coded paths; later paths will be fed into main() as inputs
-    img_path = "datasets/30_data/stack_img/img_20.tif"
-    mask_path = "datasets/30_data/mask_stack/img_20.tif"
-    
+    # img_path = "datasets/30_data/stack_img/img_20.tif"
+    # mask_path = "datasets/30_data/mask_stack/img_20.tif"
+    ####
+
     img = cv2.imread(img_path)
     normal_region = binary_thresh (img_path, mask_path)
 
@@ -43,29 +44,33 @@ def main():
     # twin_region = canny_on_twin(img_path, mask_path)
     ## twin_show = cv2.resize(twin_region, (height / 2, width / 2), interpolation = cv2.INTER_CUBIC) 
     ## cv2.imshow("twin_region", twin_show)
+    # result = twin_region 
+    ##1
 
-    
-    # result = twin_region ##1
-    
-    # print twin_region.shape, normal_region.shape
+
+    assert twin_region.shape ==  normal_region.shape      # need same shape for bitwise op
     result = cv2.bitwise_or(normal_region, twin_region)
     
     ### FOR DEBUGGING AND IMAGE SHOWING ONLY
-    height, width = result.shape[0:2]
+    # height, width = result.shape[0:2]
     # result = cv2.resize(result, (height / 2, width / 2), interpolation = cv2.INTER_CUBIC) 
     # cv2.imshow("combining twin and normal regions", result)
-    cv2.imwrite("datasets/30_data/roi/img_12_roi_result_1.tif", result)
+    ###    
 
-    # A *MUST* if use cv2.imshow to debug
-    while (1):
+    num = get_file_number(img_path)
+    name = "datasets/30_data/result_w_contours/img_" + str(num) + ".tif"
 
-        k = cv2.waitKey(1) & 0xFF
-        if (k == 27) or (k == ord("q")): 
-            print "User_int: Quit key pressed."
-            break
-    cv2.destroyAllWindows()
+    cv2.imwrite(name, result)
+    sys.exit()
 
-
+    ## DEBUG: A *MUST* if use cv2.imshow to debug
+    # while (1):
+    #     k = cv2.waitKey(1) & 0xFF
+    #     if (k == 27) or (k == ord("q")): 
+    #         print "User_int: Quit key pressed."
+    #         break
+    # cv2.destroyAllWindows()
+    ##
 
 
 # binary_thresh: apply binary_threshold to an image; 
@@ -157,36 +162,13 @@ def histo_eqlz_mask (img_path, mask_path):
     twin_region = cv2.bitwise_and(img, mask)    # ROI now selected;
     # roi_show_black = cv2.resize(twin_region, (height / 2, width / 2), interpolation = cv2.INTER_CUBIC) 
     roi_show_black = twin_region
-    cv2.imshow("roi_show_black", roi_show_black)
-    cv2.imwrite("datasets/30_data/roi/twin_black_1.tif", roi_show_black)
+    # cv2.imshow("roi_show_black", roi_show_black)
+    # cv2.imwrite("datasets/30_data/roi/twin_black_1.tif", roi_show_black)
 
 
-    # select the normal region, and mark it as white; 
-    inv_mask = cv2.bitwise_not(mask)
-    roi_show_white = cv2.bitwise_or(twin_region, inv_mask) # normal region selected; 
-                                                # turn into white; 
-    # roi_show_white = cv2.resize(roi_show_white, (height / 2, width / 2), interpolation = cv2.INTER_CUBIC) 
-    cv2.imwrite("datasets/30_data/roi/twin_white_1.tif", roi_show_white)
-
-
-    # TODO: want to change the unselected region into white, 
-        # might make equalizeHist result better
-    # twin_region = cv2.equalizeHist(unselected) 
     equalz_black = cv2.equalizeHist(roi_show_black)
-    equalz_white = cv2.equalizeHist(roi_show_white)
 
-    cv2.imwrite ("./equalz_black.tif", equalz_black)
-
-    ### Plotting; for progress report only.
-    show2 = np.hstack((equalz_black, equalz_white))
-    ## cv2.imshow("equalz_black", equalz_black)
-
-    # cv2.imshow("Equalization results: black bkg, white bkg",show2)
-
-
-    
-    img_b = equalz_black
-    img_w = equalz_white
+    img_b = equalz_black        # a shorter name
 
     # binary threshold on selected region; 
     _, img_b = cv2.threshold(img_b, 127, 255, cv2.THRESH_BINARY|cv2.THRESH_OTSU)
@@ -199,34 +181,24 @@ def histo_eqlz_mask (img_path, mask_path):
     img_b = cv2.cvtColor(img_b, cv2.COLOR_BGR2GRAY)   # convert back to gray;
 
 
-    # same as above block
-    _, img_w = cv2.threshold(img_w, 127, 255, cv2.THRESH_BINARY|cv2.THRESH_OTSU)
-            #  first return value is threshold; not needed here; 
-    _, img_contours_w, _ = cv2.findContours(img_w, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) 
-            #   return value of findContours is (im, contours, hierarchy);
-            #   only need contours, and the other 2 don't matter; 
-    img_w = cv2.cvtColor(img_w, cv2.COLOR_GRAY2BGR)   # convert to BGR
-    cv2.drawContours(img_w, img_contours_w, -1, (255,255,255), 1) # drawing all contours
-    img_w = cv2.cvtColor(img_w, cv2.COLOR_BGR2GRAY)   # convert back to gray;
-
-        
-    show3 = np.hstack((img_b, img_w))
-    ## cv2.imshow("img_b", img_b)
-    # cv2.imshow('contours: black bkg, white bkg', show3)
-
-    ### FOR DEBUGGING AND VIEWING ONLY 
-    # img = cv2.resize(img, (height / 2, width / 2), interpolation = cv2.INTER_CUBIC) 
-    # mask = cv2.resize(mask, (height / 2, width / 2), interpolation = cv2.INTER_CUBIC) 
-    # equal_1 = cv2.resize(equal_1, (height / 2, width / 2), interpolation = cv2.INTER_CUBIC) 
-
     return img_b
 
-# a helper function for normal region segmentation, aiming to reduce noises in bkg;
-# morph_gradient doesn't work well when twin region is masked out and thus black;
-# this function sets a *CURRENTLY HARDCODED* grey color, to the twin region; 
 
-# def set_gray_bkg
+# helper function, to extract file number in a path string; 
+def get_file_number(s):
+    # loop through the string;
+    num = ""
+    for i in range (0, len(s) - 1):
+        if (s[i]).isdigit():
+            num += s[i]
+    return int(num)
 
-main()
+main(sys.argv[1], sys.argv[2])
+
+# img_path = "datasets/30_data/stack_img/img_20.tif"
+# mask_path = "datasets/30_data/mask_stack/img_20.tif"
+
+
+
 
 
